1fdf1b65ed6ef936e71526a1a8c9ca90773bf0ce
BrunoDAF3 <brunokamus@gmail.com>
2024-08-20 22:52:35 -0300

    teste

---
+++ estudo-tipos-variaveis/src/TiposDeVariaveisJava.java
@@ -0,0 +1,12 @@
+public class TiposDeVariaveisJava {
+
+    public static void main(String[] args) throws Exception {
+
+        int varInteger; // variavel de 4 bytes
+        long varLong; // variavel de 8 bytes
+        float varFloat; // variavel de 4 bytes
+        double varDouble; // variavel de 8 bytes
+
+        System.out.println("Hello, World!");
+    }
+}
