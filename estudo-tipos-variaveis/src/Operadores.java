f3099d920ea73461f3670dba9a62d840daa2ab05
BrunoDAF3 <brunokamus@gmail.com>
2024-08-22 19:00:48 -0300

    salvar

---
+++ estudo-tipos-variaveis/src/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+
+        boolean falsa = false;
+
+        boolean verdadeira = true;
+
+        System.out.println(!falsa);
+        System.out.println(!verdadeira);
+    }
+
+}
